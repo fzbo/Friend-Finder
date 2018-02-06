3d1258e435b70babb9cd11278b9eb572bf73adcb
fzbo <elizabeth.bazan@gmail.com>
2018-02-06 13:30:14 -0500

    Updating friendsjs file with content

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,106 @@
-//DATA USED FOR APP WILL BE SAVED AS AN ARRAY
+//DATA OF POSSIBLE "FRIENDS" USED FOR APP WILL BE SAVED AS AN ARRAY
+// ===============================================================================
+
+
+var friends = [
+  {
+    name: "Grace Hopper",
+    photo: "https://www.kidscodecs.com/wp-content/uploads/2014/05/people-grace-hopper-mosaic-charis-tsevis.jpg",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Bessie Coleman",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/4/44/Bessie_Coleman%2C_First_African_American_Pilot_-_GPN-2004-00027.jpg",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Ada Lovelace",
+    photo: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_80%2Cw_300/MTE4MDAzNDEwODQwOTQ2MTkw/ada-lovelace-20825279-1-402.jpg",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Erna Schneider Hoover",
+    photo: "https://i.pinimg.com/736x/cb/6b/93/cb6b931604bd5f9c991a1d6f96f09e4a--november-th-spotlight.jpg",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Edith Clarke",
+    photo: "https://upload.wikimedia.org/wikipedia/en/9/92/Edith_Clarke.jpg",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: "Minnie Mouse",
+    photo: "https://www.disneyclips.com/funstuff/images/oldminnie.gif",
+    scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
+    ]
+  }
+
+];
+
+// Here we export the array. This makes it accessible to other files using require.
+module.exports = friends;
