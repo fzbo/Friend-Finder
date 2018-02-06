334c62ba5ce20e4552d935f308f69c08b4453eec
fzbo <elizabeth.bazan@gmail.com>
2018-02-06 14:07:27 -0500

    Update to routing and server files

---
+++ server.js
@@ -0,0 +1,36 @@
+
+// DEPENDENCIES
+// Npm packages that we will use to give our server useful functionality
+
+
+var express = require("express");
+var bodyParser = require("body-parser");
+
+
+// EXPRESS CONFIGURATION
+
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// BodyParser makes it easy for our server to interpret data sent to it.
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// ROUTER
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
+// LISTENER
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
